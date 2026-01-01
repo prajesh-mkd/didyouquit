8eb7916b78dc0be9946a68bdd5a208dd523e4656
Prajesh Patel <prajesh@aarshinnovations.com>
2026-01-01 01:00:03 -0500

    Configure next.config.mjs for external images

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.dicebear.com',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
