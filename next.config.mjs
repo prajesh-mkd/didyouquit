13208797dd1238a050d24a0bc70974c056b3b91e
Prajesh Patel <prajesh@aarshinnovations.com>
2026-01-01 13:33:26 -0500

    Fix Google Auth popup by setting COOP header

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,19 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin-allow-popups",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
