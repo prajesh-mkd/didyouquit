1a732acafc62de44b6dd75f88845531340f86c70
Prajesh Patel <prajesh@aarshinnovations.com>
2026-01-01 17:28:00 -0500

    UI: Update site favicon to Target icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -27,7 +27,7 @@ export default function Icon() {
                     color: "#059669", // emerald-600
                 }}
             >
-                {/* Simple FolderCheck SVG representation that fits in 32x32 */}
+                {/* Target SVG representation that fits in 32x32 */}
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -39,8 +39,9 @@ export default function Icon() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                 >
-                    <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z" />
-                    <path d="m9 13 2 2 4-4" />
+                    <circle cx="12" cy="12" r="10" />
+                    <circle cx="12" cy="12" r="6" />
+                    <circle cx="12" cy="12" r="2" />
                 </svg>
             </div>
         ),
